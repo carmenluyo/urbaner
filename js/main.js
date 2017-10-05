f3a4aee1a79842900df4e3cf65abe96d2959fa56
Carmen Luyo <c.luyorosales@gmail.com>
2017-10-04 23:42:59 -0500

    depurando js de envios

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
 $(".tabs-envios a ").on("click",function(e){
     e.preventDefault();
     $(".tabs-envios a ").removeClass("active");
     $(this).addClass("active");
     var b=$(this).attr("data-img");
-    $(".listPC").hide();
+    $(".listPC img").hide();
     $("."+b).toggle();
 })
