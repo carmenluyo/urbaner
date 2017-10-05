62d3672c971ef8d5be8d61e6fe8c4121eceb65c7
Carmen Luyo <c.luyorosales@gmail.com>
2017-10-05 17:47:48 -0500

    conectando api urbaner

--- js/main.js
+++ js/main.js
@@ -27,19 +27,18 @@ $(".tabs-envios a ").on("click",function(e){
 
 //login
 
+
 $(document).ready(function () {
-        //event handler for submit button
-        $("#myform").submit(function (e) {
-            e.preventDefault();
-            //collect userName and password entered by users
-            var userName = $("#username").val();
-            var password = $("#password").val();
+    //event handler for submit button
+    $("#myform").submit(function (e) {
+        e.preventDefault();
+        //collect userName and password entered by users
+        var userName = $("#username").val();
+        var password = $("#password").val();
 
-            //call the authenticate function
-            authenticate(userName, password);
-        });
+        //call the authenticate function
+        authenticate(userName, password);
     });
-
     //authenticate function to make ajax call
     function authenticate(userName, password) {
         $.ajax
@@ -52,9 +51,21 @@ $(document).ready(function () {
             data: {email:  userName , password :password},
 
             success: function (rs) {
-            	console.log(rs);
-
+                localStorage.setItem('info', JSON.stringify(rs));
+                console.log(rs);
                 window.location.replace("activo.html");
             }
         })
     }
+    //    var aValue = localStorage.getItem('info');
+    //    console.log(JSON.stringify(aValue));
+    var retrievedObject = localStorage.getItem('info');
+    var dt = JSON.parse(retrievedObject);
+    console.log(dt.client.email);
+    $('.name-login').html(dt.client.last_name);
+    $('#closelocal').click(function(){
+        localStorage.removeItem('info');
+        window.location.replace("http://localhost:63342/testlogin/workspace/index.html");
+    })
+
+});
