b9f9056f7dd9243c2b0291f77590ecb33b9e633b
Carmen Luyo <c.luyorosales@gmail.com>
2017-10-05 11:40:41 -0500

    add login

--- js/main.js
+++ js/main.js
@@ -6,3 +6,36 @@ $(".tabs-envios a ").on("click",function(e){
     $(".listPC img").hide();
     $("."+b).toggle();
 })
+
+$(document).ready(function () {
+      //alert('efef');
+        //event handler for submit button
+        $("#myform").submit(function (e) {
+            e.preventDefault();
+            //collect userName and password entered by users
+            var userName = $("#username").val();
+            var password = $("#password").val();
+
+            //call the authenticate function
+            authenticate(userName, password);
+        });
+    });
+
+    //authenticate function to make ajax call
+    function authenticate(userName, password) {
+        //alert('efeime');
+        $.ajax
+        ({
+            type: "POST",
+            url: "https://api.sandbox.urbaner.com/api/client/authenticate/jwt/",
+            dataType: 'json',
+            async: false,
+            //json object to sent to the authentication url
+            data: {email:  userName , password :password},
+
+            success: function (rs) {
+
+                window.location.replace("activo.html");
+            }
+        })
+    }
