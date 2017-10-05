b365ab000857bdec609309fb00cf965569391a49
Carmen Luyo <c.luyorosales@gmail.com>
2017-10-04 23:32:28 -0500

    depurando js de envios

--- js/main.js
+++ js/main.js
@@ -1,26 +1,9 @@
-$('.btnPlataform').click(function() {
-    $('.btnPlataform').addClass('activeImg');
-    $('.btnTracking').removeClass('activeImg');
-    $('.btnOptimize').removeClass('activeImg');
-    $('.imgPlataform').show();
-    $('.imgTracking').hide();
-    $('.imgOptimize').hide();
-});
 
-$('.btnTracking').click(function() {
-    $('.btnTracking').addClass('activeImg');
-    $('.btnPlataform').removeClass('activeImg');
-    $('.btnOptimize').removeClass('activeImg');
-    $('.imgTracking').show();
-    $('.imgPlataform').hide();
-    $('.imgOptimize').hide();
-});
-
-$('.btnOptimize').click(function() {
-    $('.btnOptimize').addClass('activeImg');
-    $('.btnPlataform').removeClass('activeImg');
-    $('.btnTracking').removeClass('activeImg');
-    $('.imgOptimize').show();
-    $('.imgPlataform').hide();
-    $('.imgTracking').hide();
-});
+$(".tabs-envios a ").on("click",function(e){
+    e.preventDefault();
+    $(".tabs-envios a ").removeClass("active");
+    $(this).addClass("active");
+    var b=$(this).attr("data-img");
+    $(".listPC").hide();
+    $("."+b).toggle();
+})
